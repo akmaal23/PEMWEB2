0c4677692ff2229cadb17e9277f189ebec38667a
akmaal23 <akmal.bagus0308@gmail.com>
2024-02-21 12:10:31 +0700

    fisrt commit

---
+++ praktikum 1/array_asosiatif.php
@@ -0,0 +1,9 @@
+<?php
+
+$mhs1 = ['nama' => 'Mohamad Alit Zikrillah Maulana', 
+'nim' => '0110123163', 
+"Jurusan" => 'Sistem Informasi',
+];
+
+// Memanggil array assosiatif
+echo 'Nama: ' . $mhs1['nama'];
---
+++ praktikum 1/array_assosiatif.php
@@ -0,0 +1,9 @@
+<?php
+
+$mhs1 = ['nama' => 'Akmal Bagus', 
+'nim' => '0110123159', 
+"Jurusan" => 'Sistem Informasi',
+];
+
+// Memanggil array assosiatif
+echo 'Nama: ' . $mhs1['nama'];
