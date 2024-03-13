a6fde9509d8d5b944c983303949ad8cc2fd9f4ee
akmaal23 <akmal.bagus0308@gmail.com>
2024-03-13 10:33:37 +0700

    first commit

--- praktikum 3/proses_registrasi.php
+++ praktikum 3/proses_registrasi.php
@@ -9,6 +9,30 @@ $skills = $_POST['skill'];
 $domisili = $_POST['domisili'];
 $email = $_POST['email'];
 
+//menghitung skor skills
+$total_skor = 0;
+foreach ($skills as $value){
+    $total_skor += $nilai_skill[$value];
+}
+
+//mencari kategori skill berdasarkan skor
+$kategori_skill = '';
+if ($total_skor == 0) {
+    $kategori_skill = 'Tidak memadai';
+}
+else if ($total_skor <= 40){
+    $kategori_skill = 'Kurang';
+}
+else if ($total_skor <= 60){
+    $kategori_skill = 'Cukup';
+}
+else if ($total_skor <= 100){
+    $kategori_skill = 'Baik';
+}
+else if ($total_skor <= 170){
+    $kategori_skill = 'Sangat Baik';
+}
+
 echo "NIM:  $nim";
 echo "<br> Nama: $nama";
 echo "<br> Jenis Kelamin: $jk";
